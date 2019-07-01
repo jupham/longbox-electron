c7cf283e9b5f94e2c1bbf6023bd18371e362b23d
Jordan Upham <jordan@upham.cool>
2019-06-30 23:31:03 -0400

    Got render switched over to typescript. Typescript imports working.

---
+++ src/renderer.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+
+console.log("IAMHERE");
+ReactDOM.render(<App />, document.getElementById("root"));
--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   // Put your normal webpack config below here
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js"]
+  },
   module: {
     rules: require('./webpack.rules'),
   },
